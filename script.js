0ae04e717916f18c4fabfbdb42d78ea5d8a3de82
Maciek Gurgul <margulwb@gmail.com>
2020-05-27 19:39:42 +0200

    I tried do beter scpit , more simple

--- script.js
+++ script.js
@@ -5,30 +5,38 @@ function start() {
     const head = document.querySelector(".label-head");
     const body = document.querySelector(".label-body");
     const leg = document.querySelector(".label-leg");
-    let weapons = { //i can't do reference
+    let weapons = {
         classic: ["78 at 30m<br>66 over 30m", "26 at 30m<br>22 over 30m", "26 at 30m<br>22 over 30m"],
         shorty: ["36 at 9m<br>24over 10-15m", "12 at 9m<br>3over 10-15m", "10 at 9m<br>2over 10-15m"],
         frenzy: ["78 at 20m<br>63 over 20m", "26 at 20m<br>21 over 20m", "22 at 20m<br>17 over 20m"],
+        ghost: ["105 at 30m<br>88 over 30m", "30 at 30m<br>25 over 30m", "26 at 30m<br>21 over 30m"],
     };
+    const classic = ["78 at 30m<br>66 over 30m", "26 at 30m<br>22 over 30m", "26 at 30m<br>22 over 30m"];
     clickGuns(head, body, leg, weapons);
 }
 function clickGuns(head, body, leg, weapons) {
-    const classic = document.querySelector(".classic");
-    classic.addEventListener('click', () => {
-        head.innerHTML = weapons.classic[0];
-        body.innerHTML = weapons.classic[1];
-        leg.innerHTML = weapons.classic[2];
-    });
-    const shorty = document.querySelector(".shorty");
-    shorty.addEventListener('click', () => {
-        head.innerHTML = weapons.shorty[0];
-        body.innerHTML = weapons.shorty[1];
-        leg.innerHTML = weapons.shorty[2];
-    });
-    const frenzy = document.querySelector(".frenzy");
-    frenzy.addEventListener('click', () => {
-        head.innerHTML = weapons.frenzy[0];
-        body.innerHTML = weapons.frenzy[1];
-        leg.innerHTML = weapons.frenzy[2];
-    });
+    const guns = [...document.querySelectorAll(".guns")];
+    guns.forEach(guns => guns.addEventListener('click', () => {
+        let gun = [...guns.classList];
+        let nameGun = {
+            t: gun[0]
+        }
+        if (hasOwnProperty(weapons, nameGun.t)) {
+            /*head.innerHTML = prop;
+            body.innerHTML = prop;
+            leg.innerHTML = prop;*/
+            let damage = ;
+            console.log();
+        }
+    }));
 }
+function hasOwnProperty(obj, prop) {
+    var proto = obj.__proto__ || obj.constructor.prototype;
+    return (prop in obj) && (!(prop in proto) || proto[prop] !== obj[prop]);
+    if (Object.prototype.hasOwnProperty) {
+        var hasOwnProperty = function (obj, prop) {
+            return obj.hasOwnProperty(prop);
+        }
+    }
+}
+
