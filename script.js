5202e3c2e6840e24caa2d14396820aa723fc3e28
Maciek Gurgul <margulwb@gmail.com>
2020-06-01 20:47:54 +0200

    script compleate

--- script.js
+++ script.js
@@ -11,32 +11,16 @@ function start() {
         frenzy: ["78 at 20m<br>63 over 20m", "26 at 20m<br>21 over 20m", "22 at 20m<br>17 over 20m"],
         ghost: ["105 at 30m<br>88 over 30m", "30 at 30m<br>25 over 30m", "26 at 30m<br>21 over 30m"],
     };
-    const classic = ["78 at 30m<br>66 over 30m", "26 at 30m<br>22 over 30m", "26 at 30m<br>22 over 30m"];
     clickGuns(head, body, leg, weapons);
 }
 function clickGuns(head, body, leg, weapons) {
     const guns = [...document.querySelectorAll(".guns")];
-    guns.forEach(guns => guns.addEventListener('click', () => {
-        let gun = [...guns.classList];
-        let nameGun = {
-            t: gun[0]
-        }
-        if (hasOwnProperty(weapons, nameGun.t)) {
-            /*head.innerHTML = prop;
-            body.innerHTML = prop;
-            leg.innerHTML = prop;*/
-            let damage = ;
-            console.log();
-        }
-    }));
+    guns.forEach(gun => {
+        gun.addEventListener('click', (event) => {
+            const name = event.target.innerText.toLowerCase();
+            head.innerHTML = weapons[name][0];
+            leg.innerHTML = weapons[name][2];
+            body.innerHTML = weapons[name][1];
+        });
+    });
 }
-function hasOwnProperty(obj, prop) {
-    var proto = obj.__proto__ || obj.constructor.prototype;
-    return (prop in obj) && (!(prop in proto) || proto[prop] !== obj[prop]);
-    if (Object.prototype.hasOwnProperty) {
-        var hasOwnProperty = function (obj, prop) {
-            return obj.hasOwnProperty(prop);
-        }
-    }
-}
-
