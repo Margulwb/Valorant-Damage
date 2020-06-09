3d99c96daf8c4b710ff7b7b33902f3f0029696ee
Maciek Gurgul <margulwb@gmail.com>
2020-06-09 16:37:18 +0200

    COMPLETED !! RWD isn't incredible but i try

--- script.js
+++ script.js
@@ -16,15 +16,12 @@ function start() {
         spectre: ["78 < 20m<br>66 > 20m", "26 < 20m<br>22 > 20m", "22 < 20m<br>18 > 20m"],
         bulldog: ["116", "35", "30"],
         guardian: ["195", "65", "49"],
-        phantom: ["156 <15m<br>140 (16-30m)<br>124 (31-50m)",
-            "39 <15m<br>35 (16-30m)<br>31 (31-50m)", "33 <15m<br>30 (16-30m)<br>26 (31-50m)"],
+        phantom: ["156 <15m<br>140 (16-30m)<br>124 (31-50m)", "39 <15m<br>35 (16-30m)<br>31 (31-50m)", "33 <15m<br>30 (16-30m)<br>26 (31-50m)"],
         vandal: ["156", "39", "33"],
         marshal: ["202", "101", "85"],
         operator: ["255", "150", "127"],
-        bucky: ["44 (0-8m)<br>34 (8-12m)<br>18 (12-50m)", "22 (0-8m)<br>17 (8-12m)<br>9 (12-50m)",
-            "19 (0-8m)<br>14 (8-12m)<br>8 (12-50m)"],
-        judge: ["34 (0-10m)<br>26 (10-15m)<br>20 (15-50m)", "17 (0-10m)<br>13 (10-15m)<br>10 (15-50m)",
-            "14 (0-10m)<br>11 (10-15m)<br>9 (15-50m)"],
+        bucky: ["44 (0-8m)<br>34 (8-12m)<br>18 (12-50m)", "22 (0-8m)<br>17 (8-12m)<br>9 (12-50m)", "19 (0-8m)<br>14 (8-12m)<br>8 (12-50m)"],
+        judge: ["34 (0-10m)<br>26 (10-15m)<br>20 (15-50m)", "17 (0-10m)<br>13 (10-15m)<br>10 (15-50m)", "14 (0-10m)<br>11 (10-15m)<br>9 (15-50m)"],
         ares: ["72 < 30m<br>67 > 30m", "30 < 30m<br>28 > 30m", "25 < 30m<br>23 > 30m"],
         odin: ["95 < 30m<br>77 > 30m", "38 < 30m<br>31 > 30m", "32 < 30m<br>26 > 30m"],
     };
@@ -38,7 +35,7 @@ function clickGuns(head, body, leg, weapons, score) {
             head.innerHTML = weapons[name][0];
             leg.innerHTML = weapons[name][2];
             body.innerHTML = weapons[name][1];
-            score.innerHTML = name;
+            score.innerHTML = name.toUpperCase();
         });
     });
 }
